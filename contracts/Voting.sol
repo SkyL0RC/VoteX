24c9a9e27a627194459a57724db62b5141b431d1
Ugurcoskun2005 <ugurcoskun.0804@gmail.com>
2025-02-15 21:28:02 +0300

    update vol.sol
    update

--- contracts/Voting.sol
+++ contracts/Voting.sol
@@ -1,8 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.28;
 
-import "./Sbt.sol";
-
 contract Voting {
     struct Candidate {
         string name;
@@ -16,9 +14,7 @@ contract Voting {
     uint256 public votingStart;
     uint256 public votingEnd;
 
-     SBT public sbtContract;
-
-constructor(string[] memory _candidateNames, uint256 _durationInMinutes, address _sbtAddress) {
+constructor(string[] memory _candidateNames, uint256 _durationInMinutes) {
     for (uint256 i = 0; i < _candidateNames.length; i++) {
         candidates.push(Candidate({
             name: _candidateNames[i],
@@ -29,7 +25,6 @@ constructor(string[] memory _candidateNames, uint256 _durationInMinutes, address
     votingStart = block.timestamp;
     votingEnd = block.timestamp + (_durationInMinutes * 1 minutes);
 
-    sbtContract = SBT(_sbtAddress); // SBT kontratını bağlama
 }
 
     modifier onlyOwner {
@@ -45,21 +40,22 @@ constructor(string[] memory _candidateNames, uint256 _durationInMinutes, address
     }
 
     function vote(uint256 _candidateIndex) public {
-        require(block.timestamp >= votingStart && block.timestamp < votingEnd, "Voting is not active.");
         require(!voters[msg.sender], "You have already voted.");
         require(_candidateIndex < candidates.length, "Invalid candidate index.");
         
-        // Kullanıcının SBT'ye sahip olup olmadığını kontrol et
-        require(sbtContract.hasSoul(msg.sender), "You must have an SBT to vote.");
         
-        // Oy verme işlemi
         candidates[_candidateIndex].voteCount++;
         voters[msg.sender] = true;
     }
 
+    function getAllVotesOfCandiates() public view returns (Candidate[] memory){
+        return candidates;
+    }
+
+
     function getVotingStatus() public view returns (bool) {
     return (block.timestamp >= votingStart && block.timestamp < votingEnd);
-}
+    }
 
     function getRemainingTime() public view returns (uint256) {
         require(block.timestamp >= votingStart, "Voting has not started yet.");
@@ -67,5 +63,5 @@ constructor(string[] memory _candidateNames, uint256 _durationInMinutes, address
             return 0;
         }
         return votingEnd - block.timestamp;
-}
+    }
 }
